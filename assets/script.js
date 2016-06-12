1df68d16fa563f5b32c6c3b5f5398904a81a9601
KatieLiu <katie.yiliu@gmail.com>
2016-06-12 14:43:46 -0400

    Use javascript global variables instead of HTML local storage.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+var currentState = 0;
 $(document).ready(function() {
 	if (typeof(Storage) !== "undefined") {
 		/* use this variable currentState to control what scene to show
@@ -7,12 +8,12 @@ $(document).ready(function() {
 			3 -> level 2 game page
 			4 -> level 2 finish page
 		*/
-		localStorage.currentState = 0;
+		currentState = 0;
 	}
 });
 
 function switchScene() {
-	if (localStorage.currentState == 4) { // finished game, restart
+	if (currentState == 4) { // finished game, restart
 		console.log('game finished');
 		
 		// TODO: update scene back to the start page
@@ -20,20 +21,20 @@ function switchScene() {
 		document.getElementById('page_button').innerHTML = 'START';
 		// insert top scores
 		
-		localStorage.currentState = 0;
+		currentState = 0;
 	} else {
 		console.log('change game/summary scene');
 		// switch first, then update
 		$('#transition_page').toggleClass('hide show');
 		$('#game_page').toggleClass('hide show');
-		localStorage.currentState++;
+		currentState++;
 		
-		if (localStorage.currentState == 2) {
+		if (currentState == 2) {
 			// switched to level 1 summary page
 			document.getElementById('page_title').innerHTML = ('Level# 1');
 			document.getElementById('page_button').innerHTML = 'NEXT';
 			
-		} else if (localStorage.currentState == 4) {
+		} else if (currentState == 4) {
 			// switched to level 2 summary page
 			document.getElementById('page_title').innerHTML = ('Level# 2');
 			document.getElementById('page_button').innerHTML = 'FINISH';
